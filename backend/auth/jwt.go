eee2c3ee0bce765fab908cc843baca3f6361a314
Kuja Yames <kujayames@gmail.com>
2025-03-04 23:13:55 -0500

    refactor: Organize backend into separate modules

---
+++ backend/auth/jwt.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/kujacorp/checklist/types"
+)
+
+func GenerateToken(username string, jwtKey []byte) (string, error) {
+    expirationTime := time.Now().Add(24 * time.Hour)
+    claims := &types.Claims{
+        Username: username,
+        RegisteredClaims: jwt.RegisteredClaims{
+            ExpiresAt: jwt.NewNumericDate(expirationTime),
+        },
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString(jwtKey)
+}
