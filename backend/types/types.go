52f07f2b545b43c3f22349576cc002b7916734e7
Kuja Yames <kujayames@gmail.com>
2025-03-07 23:00:34 -0500

    feat: Add backend for todo lists

--- backend/types/types.go
+++ backend/types/types.go
@@ -30,3 +30,43 @@ type SignupRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+type TodoList struct {
+	ID          int       `json:"id"`
+	Username    string    `json:"username"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type TodoListCreateRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type TodoListUpdateRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type Todo struct {
+	ID          int       `json:"id"`
+	ListID      int       `json:"list_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Completed   bool      `json:"completed"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type TodoCreateRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type TodoUpdateRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
